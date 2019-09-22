2431fe3368079c3250ae6791720945785ddb6ed2
Ahmed Al-Shahawi <alshahawi@outlook.com>
2019-09-22 22:17:18 +0200

    Update list controle partial

--- resources/views/dashboard/users/partials/control.blade.php
+++ resources/views/dashboard/users/partials/control.blade.php
@@ -3,42 +3,36 @@
         <span class="caret"></span> @lang('dashboard.lists.actions.options')
     </button>
     <ul class="dropdown-menu">
-        @if(true)
-            <li>
-                <a href="{{ route('dashboard.users.show', $user) }}">
-                    <i class="fa fa-eye"></i>
-                    @lang('dashboard.lists.actions.show')
-                </a>
-            </li>
-        @endif
-        @if(true)
-            <li>
-                <a href="{{ route('dashboard.users.edit', $user) }}">
-                    <i class="fa fa-edit"></i>
-                    @lang('dashboard.lists.actions.edit')
-                </a>
-            </li>
-        @endif
-        @if(true)
-            <li>
-                <a href="#"
-                        class="form-confirm"
-                        data-form="delete-form-users-{{ $user->getKey() }}"
-                        data-type="warning"
-                        data-title="@lang('users.dialogs.delete.title')"
-                        data-text="@lang('users.dialogs.delete.info')"
-                        data-confirm-text="@lang('users.dialogs.delete.confirm')"
-                        data-cancel-text="@lang('users.dialogs.delete.cancel')"
-                >
-                    <i class="fa fa-trash"></i>
-                    @lang('dashboard.lists.actions.delete')
-                </a>
+        <li>
+            <a href="{{ route('dashboard.users.show', $user) }}">
+                <i class="fa fa-eye"></i>
+                @lang('dashboard.lists.actions.show')
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('dashboard.users.edit', $user) }}">
+                <i class="fa fa-edit"></i>
+                @lang('dashboard.lists.actions.edit')
+            </a>
+        </li>
+        <li>
+            <a href="#"
+                    class="form-confirm"
+                    data-form="delete-form-users-{{ $user->getKey() }}"
+                    data-type="warning"
+                    data-title="@lang('users.dialogs.delete.title')"
+                    data-text="@lang('users.dialogs.delete.info')"
+                    data-confirm-text="@lang('users.dialogs.delete.confirm')"
+                    data-cancel-text="@lang('users.dialogs.delete.cancel')"
+            >
+                <i class="fa fa-trash"></i>
+                @lang('dashboard.lists.actions.delete')
+            </a>
 
-                {{ BsForm::delete(route('dashboard.users.destroy', $user), [
-                    'id' => 'delete-form-users-'.$user->getKey()
-                ]) }}
-                {{ BsForm::close() }}
-            </li>
-        @endif
+            {{ BsForm::delete(route('dashboard.users.destroy', $user), [
+                'id' => 'delete-form-users-'.$user->getKey()
+            ]) }}
+            {{ BsForm::close() }}
+        </li>
     </ul>
 </div>
